c51483242adbf5920c99142c4112d4f99a495f8c
Erick-008 <erick.s0usa@outlook.com>
2026-05-18 09:36:00 -0300

    melhorando estrutura das pastas

--- src/dao/impl/UsuarioDaoJDBC.java
+++
@@ -1,9 +0,0 @@
-package dao.impl;
-
-/**
- * Implementação JDBC de DAO de usuários.
- * Atualmente não utilizada. Pode ser usada como extensão de funcionalidades.
- */
-public class UsuarioDaoJDBC {
-    
-}
--- src/exceptions/ValidationException.java
+++
@@ -1,9 +0,0 @@
-package exceptions;
-
-/**
- * Exceção de validação. Lançada quando dados de entrada são inválidos.
- * Pode ser estendida para casos específicos de validação.
- */
-public class ValidationException {
-    
-}
